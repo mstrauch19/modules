dec1cada4e5e4903e33a8acec0b2a0cdd32e155c
root <root@localhost.localdomain>
2018-02-22 22:50:39 -0500

    final version of aux

--- .msf4/modules/auxiliary/auxiliary_mstrauch.rb
+++ .msf4/modules/auxiliary/auxiliary_mstrauch.rb
@@ -2,8 +2,6 @@ require 'socket'
 require 'msf/core'
 class MetasploitModule < Msf::Auxiliary
     include Msf::Auxiliary::Report
-    include Msf::Exploit::MSSQL
-    include Msf::Auxiliary::Scanner
 
     def initialize
         super(
@@ -18,14 +16,14 @@ class MetasploitModule < Msf::Auxiliary
 	      OptString.new('RPORT', [ true, 'Set a remote port' ])
             ], self.class)
     end
-    def run_host(ip)
-	puts "running"
-	if rhost is None or rport is None
+    def run
+	rhost = datastore['RHOST']
+	rport = datastore['RPORT']
+	if rhost == nil or rport == nil
             print_error("rhost or rport is not properly configured")
             return
         end
         s = TCPSocket.open(rhost, rport)
-       
 	begin
             line = s.gets
 	rescue Exception => e
@@ -33,22 +31,21 @@ class MetasploitModule < Msf::Auxiliary
 	    puts "No vulnerability detected"
 	end
         if line =~ /BockServe 2.0a/
-	    s.send("view")
-	    while s.gets
-		x = 5
-	    end
-	    s.send("yes")
-	    s.gets
-	    s.send("print hi")
+	    s.send("view",0)
+	    2.times{s.gets}
+	    
+	    s.send("yes",0)
+	    2.times{s.gets}
+	    s.send("print \"hi\"",0)
 	    line = s.gets
-	    if line =~ /(python -c) | (hi)/
+	    puts line
+	    if line =~ /(python -c)|(hi)/
     	        print_status("#{rhost}:#{rport} is vulnerable.")
 	    else
 		print_status("not vulnerable")
 	    end
         else 
             print_status("#{rhost}:#{rport} is not vulnerable.")
-	    print "not vulnerable"
         end
 	s.close()	
     end
